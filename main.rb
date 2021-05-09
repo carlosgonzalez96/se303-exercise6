16e66937129646dc3844f269d7bd3c2e53cb6f32
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-05-09 12:13:42 -0700

    Decompose conditional

--- main.rb
+++ main.rb
@@ -1,10 +1,8 @@
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
     if is_dark_mode
-        # darken foreground color for dark mode
-        paint(label_text, x, y, foreground_color - 10, '#111111')
+        darkModeColor(label_text, x, y, foreground_color)
     else
-        # lighten foreground color for non-dark mode
-        paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+        lightModeColor(label_text, x, y, foreground_color)
     end
 end
 
